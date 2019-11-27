da4035bffda70d2fd4c11c90b4601765a62429dc
dhn <dhn@elca.vn>
2019-11-27 17:04:12 +0700

    Second

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
-
+import { Pin } from "./Pin";
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+export class DetailViewState {
+    public Pin: Pin;
+}
